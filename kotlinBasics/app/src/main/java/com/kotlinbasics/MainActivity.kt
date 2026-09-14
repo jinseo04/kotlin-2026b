3d75590d509e82b097954960f96a0ab06f083032
jindol <choijinseo04@gmail.com>
2026-09-14 12:28:05 +0900

    kotlin 문법공부와 함수추가 했

--- kotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ kotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -28,17 +28,57 @@ class MainActivity : ComponentActivity() {
             }
         }
         week03Variables()
+        week03Functions()
     }
 }
 private fun week03Variables() {
     println("Week 03 : Variable")
 
-    val couresName = "Mobile Programming" //java final 함수 내에서 재 정의 불가능
+    val courseName = "Mobile Programming" //java final 함수 내에서 재 정의 불가능
 
-    var week = 2;
-    week = 3;
-    println("Coures : $couresName")
+    var week = 2
+    week = 3
+    println("Course : $courseName")
     println("week : $week")
+
+    println("==== kotlin Variables ====")
+
+    //val(immutable) var(variables)
+    val name = "JinDol"
+    var version = 8
+
+    println("HI $name $version")
+
+    val age: Int = 23
+    val height: Double = 178.8
+    val isStudent: Boolean = false
+
+
+    println("Age: $age. Height: $height, student: $isStudent")
+
+    var nickname:String? = null //? 안붙이면 안됨
+    nickname = "JinDol"
+    println("Nickname : $nickname ${nickname?.length}")
+
+
+
+}
+
+private fun week03Functions() {
+    fun greet(name: String): String { //return type 알려줌
+        return "Hello, $name!"
+    }
+
+    fun add(a: Int, b: Int) = a + b
+
+    fun introduce(name: String, age: Int = 19){ //디폴트 매게변수 = 지정 안할시 19로
+        println("My name is $name and I'm $age years old")
+    }
+
+    println(greet("Kotlin"))
+    println("Sum: ${add(5, -71)}")
+    introduce("Kim", 7)
+    introduce("Park")
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
